200343f166a956ae9b13f8575beca14169e2a113
Rafael Epifanio <rafael.cepifanio@gmail.com>
2020-06-05 00:18:43 -0300

    Insert OK

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
@@ -17,7 +17,12 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const transactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+    const transaction = transactionService.execute(request.body);
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
